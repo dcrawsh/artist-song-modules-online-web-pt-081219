da007ba88fc8eedaa0248c84f103b5e74375ca8d
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-20 21:28:56 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,4 +1,6 @@
 module Memorable
+  
+  module ClassMethods
   def reset_all
     self.all.clear
   end
@@ -6,5 +8,12 @@ module Memorable
   def count
   self.all.count
   end
+end 
 
+  module InstanceMethods
+    def initialize
+    self.class.all << self 
+  end 
+
+end 
 end 
