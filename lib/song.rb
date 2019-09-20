3d6d106e7f05429c4569ab2e6cc53419d7fcbbff
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-20 21:16:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  extend Paramable
   attr_accessor :name
   attr_reader :artist
 
@@ -20,7 +21,5 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  
 end
